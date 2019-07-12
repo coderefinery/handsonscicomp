15e70623b2b13e1df1d17ed3b182ff8dcf262d3d
Richard Darst <rkd@zgib.net>
2019-07-12 13:07:55 +0300

    yaml-table.py: Add substitutions and local info

--- _ext/yaml_table.py
+++ _ext/yaml_table.py
@@ -11,6 +11,7 @@ from docutils import nodes
 from docutils import statemachine
 from docutils.parsers.rst.directives.tables import CSVTable, ListTable
 
+from sphinx_ext_substitution import make_sub_rst
 
 
 class YamlTable(CSVTable):
@@ -43,19 +44,39 @@ class YamlTable(CSVTable):
 
 class CourseTable(YamlTable):
     def transform_yaml(self, data):
-        data.insert(0, ["", "About", "Video Intro", "Reading"])
-
+        newrows = [ ]
+        self.ids = [ ]
+        newrows.append(["", "About", "Video Intro", "Reading", make_sub_rst('site-name', "")])
         for i, row in enumerate(data):
-            if isinstance(row, dict):
-                row = [
-                    row.get('id', ''),
-                    row.get('desc', ''),
-                    row.get('video', ''),
-                    row.get('reading', ''),
-                    #row.get('exercises', ''),
-                    ]
-                data[i] = row
-        return data
+            id_ = row['id'].split()[0]
+            self.ids.append(id_)
+            row = [
+                row.get('id', ''),
+                make_sub_rst(id_+'-desc', row.get('desc', '')),
+                make_sub_rst(id_+'-video', row.get('video', '')),
+                make_sub_rst(id_+'-reading', row.get('reading', '')),
+                make_sub_rst(id_+'-local', ""),
+                #row.get('exercises', ''),
+            ]
+            print(row)
+            newrows.append(row)
+        return newrows
+    def run(self):
+        table_and_messages = super(CourseTable, self).run()
+        table = table_and_messages[0]
+        tgroup = table[0]
+        tbody = tgroup[-1]
+        print(tbody)
+        for i, row in enumerate(tbody):
+            row['ids'].append(self.ids[i])
+            row[0]['classes'].append('row-name')
+            row[1]['classes'].append('row-desc')
+            row[2]['classes'].append('row-video')
+            row[3]['classes'].append('row-reading')
+            row[4]['classes'].append('row-local')
+            #row[4]['style'].append('row-name')
+        return table_and_messages
+
 
 
 
