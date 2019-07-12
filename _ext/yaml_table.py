00ca0039b527b67195906b93b2bfd03f19886bd7
Richard Darst <rkd@zgib.net>
2019-07-12 16:35:46 +0300

    Add 'local' column only if 'site-name' is defined

--- _ext/yaml_table.py
+++ _ext/yaml_table.py
@@ -11,7 +11,7 @@ from docutils import nodes
 from docutils import statemachine
 from docutils.parsers.rst.directives.tables import CSVTable, ListTable
 
-from sphinx_ext_substitution import make_sub_rst
+from sphinx_ext_substitution import make_sub_rst, get_substitutions
 
 
 class YamlTable(CSVTable):
@@ -44,9 +44,14 @@ class YamlTable(CSVTable):
 
 class CourseTable(YamlTable):
     def transform_yaml(self, data):
+        subs = get_substitutions(self.state.document.settings.env.config)
+        has_local = 'site-name' in subs
+
         newrows = [ ]
         self.ids = [ ]
-        newrows.append(["", "About", "Video Intro", "Reading", make_sub_rst('site-name', "")])
+        newrows.append(["", "About", "Video Intro", "Reading", ])
+        if has_local:
+            newrows[-1].append(make_sub_rst('site-name', ""))
         for i, row in enumerate(data):
             id_ = row['id'].split()[0]
             self.ids.append(id_)
@@ -55,9 +60,11 @@ class CourseTable(YamlTable):
                 make_sub_rst(id_+'-desc', row.get('desc', '')),
                 make_sub_rst(id_+'-video', row.get('video', '')),
                 make_sub_rst(id_+'-reading', row.get('reading', '')),
-                make_sub_rst(id_+'-local', ""),
                 #row.get('exercises', ''),
             ]
+            if has_local:
+                row.append(make_sub_rst(id_+'-local', ""))
+
             newrows.append(row)
         return newrows
     def run(self):
@@ -71,8 +78,10 @@ class CourseTable(YamlTable):
             row[1]['classes'].append('row-desc')
             row[2]['classes'].append('row-video')
             row[3]['classes'].append('row-reading')
-            row[4]['classes'].append('row-local')
-            #row[4]['style'].append('row-name')
+            if len(row) > 4:
+                row[4]['classes'].append('row-local')
+            #if len(row) > 5:
+            #    row[4]['style'].append('row-name')
         return table_and_messages
 
 
